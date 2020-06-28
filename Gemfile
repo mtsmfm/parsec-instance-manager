a8ae11be8cc9eca241d11276dc4fa3b83e77eeb9
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2020-06-28 09:48:07 +0000

    $ bin/rails g graphql:install --skip-graphiql --batch

--- Gemfile
+++ Gemfile
@@ -28,3 +28,4 @@ group :test do
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
+gem 'graphql-batch'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  post "/graphql", to: "graphql#execute"
   root to: 'root#index'
 end
