267c06d25e19fce724ffe11c08aac03fc93ac821
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2020-07-06 12:42:17 +0000

    Read PARSEC_SESSION_ID from secret manager

--- app/models/parsec_api_client.rb
+++ app/models/parsec_api_client.rb
@@ -2,6 +2,10 @@ class ParsecApiClient
   class Error < RuntimeError; end
 
   def list_hosts
+    client = Google::Cloud::SecretManager.secret_manager_service
+    name = client.secret_version_path(project: ENV["FIREBASE_PROJECT_ID"], secret: "PARSEC_SESSION_ID", secret_version: "latest")
+    parsec_session_id = client.access_secret_version(name: name).payload.data
+
     uri = URI('https://kessel-api.parsecgaming.com/hosts/')
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
