93b8e3aa0e0206c37151369786e8a656e980bf40
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2020-07-08 15:37:06 +0000

    Read from secret manager

--- app/models/parsec_api_client.rb
+++ app/models/parsec_api_client.rb
@@ -9,7 +9,7 @@ class ParsecApiClient
     uri = URI('https://kessel-api.parsecgaming.com/hosts/')
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    req = Net::HTTP::Get.new(uri.path, 'Content-Type' => 'application/json', 'Authorization' => 'Bearer ' + ENV['PARSEC_SESSION_ID'])
+    req = Net::HTTP::Get.new(uri.path, 'Content-Type' => 'application/json', 'Authorization' => 'Bearer ' + parsec_session_id)
     res = http.request(req)
     if res.code == "200"
       JSON.parse(res.body, symbolize_names: true)[:data]
