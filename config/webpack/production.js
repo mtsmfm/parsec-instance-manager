9f1cfa5f27345f2581537eb702a488471cd236df
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2020-07-08 15:06:00 +0000

    Disable brotil

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+environment.plugins.delete('Compression Brotli')
+
 module.exports = environment.toWebpackConfig()
